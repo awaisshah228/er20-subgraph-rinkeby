23626bad5be043c9f51186ee46707b3258dd964b
awaisshah228 <m.awaisshah228.com>
2022-07-07 16:07:34 +0500

    index using graph erc20 on rinkeby

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigDecimal, BigInt, ethereum } from "@graphprotocol/graph-ts"
 import {
   Test,
   Approval,
@@ -6,64 +6,132 @@ import {
   Transfer,
   UpdateTreasuryWallet
 } from "../generated/Test/Test"
-import { ExampleEntity } from "../generated/schema"
+// import { ExampleEntity } from "../generated/schema"
+import {
+  Account,
+  Token,
+  TokenApproval,
+  TokenBalance,
+} from "../generated/schema";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+const zeroAddress = '0x0000000000000000000000000000000000000000';
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+function loadOrCreateAccount(address: string): Account | null {
+  let account = Account.load(address);
+  if (!account) {
+    account = new Account(address);
+    account.save();
   }
+  return account;
+}
+function loadOrCreateToken(event: ethereum.Event): Token | null {
+  let token = Token.load(event.address.toHex());
+  if (!token) {
+    let erc20 = Test.bind(event.address);
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+    let nameResult = erc20.try_name();
+    if (nameResult.reverted) {
+      return null;
+    }
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
+    let symbolResult = erc20.try_symbol();
+    if (symbolResult.reverted) {
+      return null;
+    }
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+    let decimalsResult = erc20.try_decimals();
+    if (decimalsResult.reverted) {
+      return null;
+    }
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+    // Ignore any weird tokens to avoid overflowing the `decimals` field (which is an i32)
+    // On mainnet for example there is at least one token which has a huge value for `decimals`
+    // and that would overflow the Token entity's i32 field for the decimals
+    // if (decimalsResult.value.toBigDecimal().gt(BigDecimal.fromString("255"))) {
+    //   return null;
+    // }
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.increaseAllowance(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
-  // - contract.treasuryWallet(...)
+    token = new Token(event.address.toHex());
+    token.name = nameResult.value;
+    token.symbol = symbolResult.value;
+    token.decimals = decimalsResult.value;
+    token.save();
+  }
+  return token;
 }
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleApproval(event: Approval): void {
+  let token = loadOrCreateToken(event);
+  if (!token) {
+    return;
+  }
 
-export function handleTransfer(event: Transfer): void {}
+  let owner = event.params.owner.toHex();
+  let spender = event.params.spender.toHex();
+  let value = event.params.value.toBigDecimal();
 
-export function handleUpdateTreasuryWallet(event: UpdateTreasuryWallet): void {}
+  let ownerAccount = loadOrCreateAccount(owner);
+  let spenderAccount = loadOrCreateAccount(spender);
+
+  if (!ownerAccount || !spenderAccount) {
+    return;
+  }
+
+  let tokenApproval = TokenApproval.load(
+    token.id + "-" + ownerAccount.id + "-" + spenderAccount.id
+  );
+  if (!tokenApproval) {
+    tokenApproval = new TokenApproval(
+      token.id + "-" + ownerAccount.id + "-" + spenderAccount.id
+    );
+    tokenApproval.token = token.id;
+    tokenApproval.ownerAccount = ownerAccount.id;
+    tokenApproval.spenderAccount = spenderAccount.id;
+  }
+  tokenApproval.value = value;
+  tokenApproval.save();
+}
+
+
+
+export function handleTransfer(event: Transfer): void {
+  let token = loadOrCreateToken(event);
+  if (!token) {
+    return;
+  }
+
+  let from = event.params.from.toHex();
+  let to = event.params.to.toHex();
+  let value = event.params.value.toBigDecimal();
+
+  let fromAccount = loadOrCreateAccount(from);
+  let toAccount = loadOrCreateAccount(to);
+
+  if (!fromAccount || !toAccount) {
+    return;
+  }
+
+  if (fromAccount.id != zeroAddress) {
+    let fromTokenBalance = TokenBalance.load(token.id + "-" + fromAccount.id);
+    if (!fromTokenBalance) {
+      fromTokenBalance = new TokenBalance(token.id + "-" + fromAccount.id);
+      fromTokenBalance.token = token.id;
+      fromTokenBalance.account = fromAccount.id;
+      fromTokenBalance.value = BigDecimal.fromString("0");
+    }
+    fromTokenBalance.value = fromTokenBalance.value.minus(value);
+    fromTokenBalance.save();
+  }
+
+  let toTokenBalance = TokenBalance.load(token.id + "-" + toAccount.id);
+  if (!toTokenBalance) {
+    toTokenBalance = new TokenBalance(token.id + "-" + toAccount.id);
+    toTokenBalance.token = token.id;
+    toTokenBalance.account = toAccount.id;
+    toTokenBalance.value = BigDecimal.fromString("0");
+  }
+  toTokenBalance.value = toTokenBalance.value.plus(value);
+  toTokenBalance.save();
+}
+
+
